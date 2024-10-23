ad759032f9160cb67767e67fb39e2fa072af70e9
SanketTops <sanket.tops@gmail.com>
2024-10-23 16:40:24 +0530

    Collection Type

---
+++ Assignments/Module-2_CollectionType/dict_example.py
@@ -0,0 +1,46 @@
+stdata={'id':101,'name':'sanket','city':'rajkot'}
+"""print(stdata)
+print(stdata['name'])
+print(stdata.get('city'))
+print(stdata.keys())
+print(stdata.values())
+print(len(stdata))"""
+
+# ----------------------------------- #
+"""if 'name' in stdata:
+    print("Yes...")
+else:
+    print("Noo")
+"""
+
+"""if 'sanket' in stdata.values():
+    print("Yes...")
+else:
+    print("Noo")"""
+
+# ------------------------------------ #
+#print(stdata)
+
+"""for i in stdata:
+    print(i)"""
+
+"""for i in stdata.values():
+    print(i)"""
+
+"""for i in stdata.items():
+    print(i)"""
+
+"""for i,j in stdata.items():
+    #print(i,j)
+    print(f"Key={i} and Value={j}")"""
+
+# ------------------------------------ #
+print(stdata)
+
+#stdata["sub"]="Python" #new pair add
+#stdata['id']=102 #update value
+#stdata.pop('city')
+#stdata.clear()
+#del stdata
+
+print(stdata)
---
+++ Assignments/Module-2_CollectionType/set_exam.py
@@ -0,0 +1,24 @@
+myset={'a','b','c','d','e'}
+
+#print(myset)
+#print(len(myset))
+
+"""if 'e' in myset:
+    print("Yes..")
+else:
+    print("Noo")"""
+
+# -------------------------------------- #
+print(myset)
+#myset.add('f')
+#myset.update(['g','h','i'])
+#myset.remove('c')
+#myset.pop()
+#print(myset)
+
+newset={'p','q','r','s','a','b'}
+print(newset)
+
+#x=myset.union(newset)
+x=myset.intersection(newset)
+print(x)
