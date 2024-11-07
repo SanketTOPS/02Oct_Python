daafb8f534ba7d47aae845779c0c24dc46347b94
SanketTops <sanket.tops@gmail.com>
2024-11-07 16:26:11 +0530

    Modules

---
+++ Assignments/Module-2_UDF_Rev/default_arg.py
@@ -0,0 +1,7 @@
+def getdata(id,name,city='Rajkot'): #default argu.
+    print("ID:",id)
+    print("Name:",name)
+    print("City:",city)
+
+#getdata(101,'Mitesh')
+getdata(101,'Ashok','Baroda')
---
+++ Assignments/Module-2_UDF_Rev/keyword_argu.py
@@ -0,0 +1,11 @@
+def getdata(id,name,sub):
+    print("ID:",id)
+    print("Name:",name)
+    print("Sub:",sub)
+
+
+#getdata(101,'Sanket','Python')
+
+#getdata('Sanket',101,'Python') #positional arguments
+
+getdata(id=101,sub='iOS',name='Nirav') #keyword arguments
