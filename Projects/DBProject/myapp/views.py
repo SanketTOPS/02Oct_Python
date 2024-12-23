31202413ea609fdf476472ec1de47b11d4da337c
SanketTops <sanket.tops@gmail.com>
2024-12-23 14:57:42 +0530

    Insert Ope.

--- Projects/DBProject/myapp/views.py
+++ Projects/DBProject/myapp/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from .forms import *
 
 # Create your views here.
 def index(request):
+    if request.method=='POST':
+        stdata=studData(request.POST)
+        if stdata.is_valid():
+            stdata.save()
+            print("Data inserted!")
+        else:
+            print(stdata.errors)
     return render(request,'index.html')
