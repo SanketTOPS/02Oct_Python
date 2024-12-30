69cf3eae20667d83ee600f41ae0db9e2567d6b87
SanketTops <sanket.tops@gmail.com>
2024-12-30 14:58:16 +0530

    Full Auth App

--- Projects/AuthProject/myapp/views.py
+++ Projects/AuthProject/myapp/views.py
@@ -1,17 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .forms import *
+from django.contrib.auth import logout
 
 # Create your views here.
 
 def index(request): #Login Page
-    return render(request,'index.html')
+    msg=""
+    if request.method=='POST':
+        unm=request.POST['username']
+        pas=request.POST['password']
+
+        user=usersignup.objects.filter(username=unm,password=pas)
+        if user: #TRUE
+            print("Login Successfully!")
+            msg="Login Successfully!"
+            request.session['user']=unm #create a session
+            return redirect('home')
+        else:
+            print("Error!Login faild...")
+            msg="Login faild..."
+    return render(request,'index.html',{'msg':msg})
 
 def signup(request): #Signup Page
+    msg=""
     if request.method=='POST':
         newuser=signupForm(request.POST)
         if newuser.is_valid():
             newuser.save()
             print("Signup Successfully!")
+            msg="Signup Successfully!"
+            return redirect('/')
         else:
             print(newuser.errors)
-    return render(request,'signup.html')
+            msg="Something went wrong..."
+    return render(request,'signup.html',{'msg':msg})
+
+def home(request):
+    user=request.session.get('user')
+    return render(request,'home.html',{'user':user})
+
+def userlogout(request):
+    logout(request)
+    return redirect('/')
