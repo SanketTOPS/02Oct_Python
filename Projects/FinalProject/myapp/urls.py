d9da533c76ffe4a866aa71b87e84397b9b690de4
SanketTops <sanket.tops@gmail.com>
2025-01-04 14:48:27 +0530

    Pages

--- Projects/FinalProject/myapp/urls.py
+++ Projects/FinalProject/myapp/urls.py
@@ -4,4 +4,9 @@ from myapp import views
 
 urlpatterns = [
    path('',views.index),
+   path('signup/',views.signup,name='signup'),
+   path('notes/',views.notes,name='notes'),
+   path('profile/',views.profile,name='profile'),
+   path('about/',views.about,name='about'),
+   path('contact/',views.contact,name='contact'),
 ]
--- Projects/FinalProject/myapp/views.py
+++ Projects/FinalProject/myapp/views.py
@@ -3,3 +3,18 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+
+def signup(request):
+    return render(request,'signup.html')
+
+def notes(request):
+    return render(request,'notes.html')
+
+def profile(request):
+    return render(request,'profile.html')
+
+def about(request):
+    return render(request,'about.html')
+
+def contact(request):
+    return render(request,'contact.html')
