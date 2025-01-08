dc0cd7c642b02be41e77aa0c6921ddd0086f8c63
SanketTops <sanket.tops@gmail.com>
2025-01-08 14:50:58 +0530

    Full Auth

--- Projects/FinalProject/myapp/urls.py
+++ Projects/FinalProject/myapp/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
    path('profile/',views.profile,name='profile'),
    path('about/',views.about,name='about'),
    path('contact/',views.contact,name='contact'),
+   path('userlogout/',views.userlogout),
 ]
--- Projects/FinalProject/myapp/views.py
+++ Projects/FinalProject/myapp/views.py
@@ -1,16 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import *
+from django.contrib.auth import logout
 
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    msg=""
+    user=request.session.get('user')
+    if request.method=='POST':
+        unm=request.POST['username']
+        pas=request.POST['password']
+
+        user=userSignup.objects.filter(username=unm,password=pas)
+        if user: #TRUE
+            print("Login Successfull!")
+            request.session['user']=unm #session
+            return redirect('notes')
+        else:
+            print("Error!Login Faild...")
+            msg="Error!Login Faild..."
+    return render(request,'index.html',{'msg':msg,'user':user})
 
 def signup(request):
-    return render(request,'signup.html')
+    msg=""
+    if request.method=='POST':
+        newuser=signupForm(request.POST)
+        if newuser.is_valid():
+            newuser.save()
+            return redirect('/')
+        else:
+            print(newuser.errors)
+            msg="Error!Something went wrong...."
+    return render(request,'signup.html',{'msg':msg})
 
 def notes(request):
-    return render(request,'notes.html')
+    user=request.session.get('user')
+    return render(request,'notes.html',{'user':user})
 
 def profile(request):
+    
     return render(request,'profile.html')
 
 def about(request):
@@ -18,3 +45,7 @@ def about(request):
 
 def contact(request):
     return render(request,'contact.html')
+
+def userlogout(request):
+    logout(request)
+    return redirect('/')
