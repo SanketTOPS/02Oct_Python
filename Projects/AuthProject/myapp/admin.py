423e0dffc8b2bd25a8a1474aaf1b766545cc6e28
SanketTops <sanket.tops@gmail.com>
2025-01-30 14:18:23 +0530

    Final

--- Projects/AuthProject/myapp/admin.py
+++ Projects/AuthProject/myapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
+
 
 # Register your models here.
+class signupData(admin.ModelAdmin):
+    list_display = ["id", "firstname", "lastname", "username", "city", "mobile"]
+
+
+admin.site.register(usersignup, signupData)
--- Projects/AuthProject/myapp/views.py
+++ Projects/AuthProject/myapp/views.py
@@ -1,44 +1,48 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from .forms import *
 from django.contrib.auth import logout
 
 # Create your views here.
 
-def index(request): #Login Page
-    msg=""
-    if request.method=='POST':
-        unm=request.POST['username']
-        pas=request.POST['password']
 
-        user=usersignup.objects.filter(username=unm,password=pas)
-        if user: #TRUE
+def index(request):  # Login Page
+    msg = ""
+    if request.method == "POST":
+        unm = request.POST["username"]
+        pas = request.POST["password"]
+
+        user = usersignup.objects.filter(username=unm, password=pas)
+        if user:  # TRUE
             print("Login Successfully!")
-            msg="Login Successfully!"
-            request.session['user']=unm #create a session
-            return redirect('home')
+            msg = "Login Successfully!"
+            request.session["user"] = unm  # create a session
+            return redirect("home")
         else:
             print("Error!Login faild...")
-            msg="Login faild..."
-    return render(request,'index.html',{'msg':msg})
+            msg = "Login faild..."
+    return render(request, "index.html", {"msg": msg})
 
-def signup(request): #Signup Page
-    msg=""
-    if request.method=='POST':
-        newuser=signupForm(request.POST)
+
+def signup(request):  # Signup Page
+    msg = ""
+    if request.method == "POST":
+        newuser = signupForm(request.POST)
         if newuser.is_valid():
             newuser.save()
             print("Signup Successfully!")
-            msg="Signup Successfully!"
-            return redirect('/')
+            msg = "Signup Successfully!"
+            return redirect("/")
         else:
             print(newuser.errors)
-            msg="Something went wrong..."
-    return render(request,'signup.html',{'msg':msg})
+            msg = "Something went wrong..."
+    return render(request, "signup.html", {"msg": msg})
+
 
 def home(request):
-    user=request.session.get('user')
-    return render(request,'home.html',{'user':user})
+    user = request.session.get("user")
+    return render(request, "home.html", {"user": user})
+
 
 def userlogout(request):
     logout(request)
-    return redirect('/')
+    return redirect("/")
